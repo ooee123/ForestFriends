1685f092bdb324b6160d52a1f0cef3bb5946f220
lisayip <imlisayip@gmail.com>
2015-04-15 08:59:06 -0700

    Create Simple Pi GUI using Python Tkinter

---
+++ piGUI/gui.py
@@ -0,0 +1,13 @@
+# Raspberry Pi GUI
+
+from Tkinter import *
+
+# Create the window
+root = Tk()
+
+# Modify root window
+root.title("Raspberry Pi GUI")
+root.geometry("320x240")
+
+#kick off the event loop
+root.mainloop()
