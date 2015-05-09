183d222b0e3c2e210e9256dbde286c4fea8648a5
lisayip <imlisayip@gmail.com>
2015-05-09 16:30:40 -0700

    modified T character

--- pathConversion/letter/T.java
+++ pathConversion/letter/T.java
@@ -8,9 +8,9 @@ public class T implements Letter
    {
       Paths paths = new Paths(Dimensions.WIDTH, Dimensions.HEIGHT, "T");
       paths.add(new Path(Dimensions.MIN_WIDTH, Dimensions.MAX_HEIGHT, Path.MovementType.START));
-      paths.add(new Path(Dimensions.MAX_WIDTH, Dimensions.MAX_HEIGHT, Path.MovementType.LINE));
-      paths.add(new Path(Dimensions.MAX_WIDTH / 2, Dimensions.MIN_HEIGHT, Path.MovementType.MOVE));
-      paths.add(new Path(Dimensions.MAX_WIDTH / 2, Dimensions.MAX_HEIGHT, Path.MovementType.LINE));
+      paths.add(new Path(Dimensions.getRelativeX(4, 5), Dimensions.MAX_HEIGHT, Path.MovementType.LINE));
+      paths.add(new Path(Dimensions.getRelativeX(2, 5), Dimensions.MIN_HEIGHT, Path.MovementType.MOVE));
+      paths.add(new Path(Dimensions.getRelativeX(2, 5), Dimensions.MAX_HEIGHT, Path.MovementType.LINE));
       return paths;
    }
 }
