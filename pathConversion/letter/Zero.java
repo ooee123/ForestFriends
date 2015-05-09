9e776c2a3739a566ec03f18c005d69dae831cd12
lisayip <imlisayip@gmail.com>
2015-05-09 16:40:47 -0700

    Copied letter O as zero

---
+++ pathConversion/letter/Zero.java
@@ -0,0 +1,22 @@
+package letter;
+
+import java.util.*;
+
+public class O implements Letter
+{
+   public Paths getPaths()
+   {
+      Paths paths = new Paths(Dimensions.WIDTH, Dimensions.HEIGHT, "O");
+      paths.add(new Path(Dimensions.MAX_WIDTH, Dimensions.MAX_HEIGHT / 2, Path.MovementType.START));
+      int centerX = Dimensions.MAX_WIDTH / 2;
+      int centerY = Dimensions.MAX_HEIGHT / 2;
+      for (int point = 1; point <= Letter.DISCRETE_POINTS; point++)
+      {
+         int x = (int)Math.round(centerX + Math.cos(point * 2 * Math.PI / Letter.DISCRETE_POINTS) * Dimensions.MAX_WIDTH / 2);
+         int y = (int)Math.round(centerY + Math.sin(point * 2 * Math.PI / Letter.DISCRETE_POINTS) * Dimensions.MAX_HEIGHT / 2);
+         paths.add(new Path(x, y, Path.MovementType.LINE));
+      }
+
+      return paths;
+   }
+}
