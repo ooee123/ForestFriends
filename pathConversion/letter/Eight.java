00488a52f241d00e26edc6f86be6d46ec0e91a8c
lisayip <imlisayip@gmail.com>
2015-05-12 11:25:56 -0700

    Created number 8

---
+++ pathConversion/letter/Eight.java
@@ -0,0 +1,46 @@
+package letter;
+
+import java.util.*;
+
+public class Eight implements Letter
+{
+   public static final int CENTER_X = Dimensions.getRelativeX(Dimensions.MAX_WIDTH_80, 1, 2);
+
+   public static final int FIRST_STARTING_ANGLE = Letter.DISCRETE_POINTS * 270/360; 
+   public static final int FIRST_ENDING_ANGLE = Letter.DISCRETE_POINTS * 630/360; 
+   public static final int FIRST_RADIUS_X = Dimensions.getRelativeX(Dimensions.MAX_WIDTH_80, 4, 9);
+   public static final int FIRST_RADIUS_Y = Dimensions.getRelativeY(7, 32);
+
+   public static final int SECOND_ENDING_ANGLE = Letter.DISCRETE_POINTS * 90/360;
+   public static final int SECOND_RADIUS_X = Dimensions.getRelativeX(Dimensions.MAX_WIDTH_80, 1, 2);
+
+   public static final int SECOND_STARTING_ANGLE = Letter.DISCRETE_POINTS * 450/360;
+   public static final int FIRST_CENTER_X = CENTER_X;
+   public static final int FIRST_CENTER_Y = Dimensions.MAX_HEIGHT - FIRST_RADIUS_Y;
+   public static final int SECOND_CENTER_X = CENTER_X;
+   public static final int SECOND_RADIUS_Y = (Dimensions.MAX_HEIGHT - 2 * FIRST_RADIUS_Y) / 2;
+   public static final int SECOND_CENTER_Y = FIRST_CENTER_Y - FIRST_RADIUS_Y - SECOND_RADIUS_Y;
+
+
+   public Paths getPaths()
+   {
+      Paths paths = new Paths(Dimensions.WIDTH_80, Dimensions.HEIGHT, "8");
+      int point = FIRST_STARTING_ANGLE;
+      int x = (int)Math.round(FIRST_CENTER_X + Math.cos(point * 2 * Math.PI / Letter.DISCRETE_POINTS) * FIRST_RADIUS_X);
+      int y = (int)Math.round(FIRST_CENTER_Y + Math.sin(point * 2 * Math.PI / Letter.DISCRETE_POINTS) * FIRST_RADIUS_Y);
+      paths.add(new Path(x, y, Path.MovementType.START));
+      for (point++; point <= FIRST_ENDING_ANGLE; point++)
+      {
+         x = (int)Math.round(FIRST_CENTER_X + Math.cos(point * 2 * Math.PI / Letter.DISCRETE_POINTS) * FIRST_RADIUS_X);
+         y = (int)Math.round(FIRST_CENTER_Y + Math.sin(point * 2 * Math.PI / Letter.DISCRETE_POINTS) * FIRST_RADIUS_Y);
+         paths.add(new Path(x, y, Path.MovementType.LINE));
+      }
+      for (point = SECOND_STARTING_ANGLE; point >= SECOND_ENDING_ANGLE; point--)
+      {
+         x = (int)Math.round(SECOND_CENTER_X + Math.cos(point * 2 * Math.PI / Letter.DISCRETE_POINTS) * SECOND_RADIUS_X);
+         y = (int)Math.round(SECOND_CENTER_Y + Math.sin(point * 2 * Math.PI / Letter.DISCRETE_POINTS) * SECOND_RADIUS_Y);
+         paths.add(new Path(x, y, Path.MovementType.LINE));
+      }
+      return paths;
+   }
+}
