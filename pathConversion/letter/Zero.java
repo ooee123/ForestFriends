f4af68901d62af129fddf8462b2f1bf4043696fe
lisayip <imlisayip@gmail.com>
2015-05-12 12:15:16 -0700

    changed Zero to smaller radius, same as letter o

--- pathConversion/letter/Zero.java
+++ pathConversion/letter/Zero.java
@@ -6,14 +6,16 @@ public class Zero implements Letter
 {
    public Paths getPaths()
    {
-      Paths paths = new Paths(Dimensions.WIDTH, Dimensions.HEIGHT, "0");
-      paths.add(new Path(Dimensions.MAX_WIDTH, Dimensions.MAX_HEIGHT / 2, Path.MovementType.START));
-      int centerX = Dimensions.MAX_WIDTH / 2;
-      int centerY = Dimensions.MAX_HEIGHT / 2;
+      Paths paths = new Paths(Dimensions.WIDTH_90, Dimensions.HEIGHT, "0");
+      paths.add(new Path(Dimensions.MAX_WIDTH_90, Dimensions.getRelativeY(1, 2), Path.MovementType.START));
+      int centerX = Dimensions.getRelativeX(Dimensions.MAX_WIDTH_90, 1, 2);
+      int centerY = Dimensions.getRelativeY(1, 2);
+      final int radiusX = Dimensions.getRelativeX(Dimensions.MAX_WIDTH_90, 1, 2);
+      final int radiusY = Dimensions.getRelativeY(1, 2);
       for (int point = 1; point <= Letter.DISCRETE_POINTS; point++)
       {
-         int x = (int)Math.round(centerX + Math.cos(point * 2 * Math.PI / Letter.DISCRETE_POINTS) * Dimensions.MAX_WIDTH / 2);
-         int y = (int)Math.round(centerY + Math.sin(point * 2 * Math.PI / Letter.DISCRETE_POINTS) * Dimensions.MAX_HEIGHT / 2);
+         int x = (int)Math.round(centerX + Math.cos(point * 2 * Math.PI / Letter.DISCRETE_POINTS) * radiusX);
+         int y = (int)Math.round(centerY + Math.sin(point * 2 * Math.PI / Letter.DISCRETE_POINTS) * radiusY);
          paths.add(new Path(x, y, Path.MovementType.LINE));
       }
 
