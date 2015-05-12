fe54964a92d7faa16625f6084440438477f51849
lisayip <imlisayip@gmail.com>
2015-05-12 10:44:13 -0700

    Created number four with _90

--- pathConversion/letter/Four.java
+++ pathConversion/letter/Four.java
@@ -6,11 +6,11 @@ public class Four implements Letter
 {
    public Paths getPaths()
    {
-      Paths paths = new Paths(Dimensions.WIDTH, Dimensions.HEIGHT, "4");
-      paths.add(new Path(Dimensions.MAX_WIDTH * 4 / 5, Dimensions.MIN_HEIGHT, Path.MovementType.START));
-      paths.add(new Path(Dimensions.MAX_WIDTH * 4 / 5, Dimensions.MAX_HEIGHT, Path.MovementType.LINE));
-      paths.add(new Path(Dimensions.MIN_WIDTH, Dimensions.MAX_HEIGHT / 3, Path.MovementType.LINE));
-      paths.add(new Path(Dimensions.MAX_WIDTH, Dimensions.MAX_HEIGHT / 3, Path.MovementType.LINE));
+      Paths paths = new Paths(Dimensions.WIDTH_90, Dimensions.HEIGHT, "4");
+      paths.add(new Path(Dimensions.getRelativeX(Dimensions.MAX_WIDTH_90, 4, 5), Dimensions.MIN_HEIGHT, Path.MovementType.START));
+      paths.add(new Path(Dimensions.getRelativeX(Dimensions.MAX_WIDTH_90, 4, 5), Dimensions.MAX_HEIGHT, Path.MovementType.LINE));
+      paths.add(new Path(Dimensions.MIN_WIDTH, Dimensions.getRelativeY(1 , 3), Path.MovementType.LINE));
+      paths.add(new Path(Dimensions.MAX_WIDTH_90, Dimensions.getRelativeY(1,  3), Path.MovementType.LINE));
       return paths;
    }
 }
